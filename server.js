7898614632aa77fbd486b9037a213f77097a8c53
ianchenx <nay99@foxmail.com>
2025-05-27 11:11:52 +0800

    support proxy

--- server.js
+++ server.js
@@ -69,6 +69,19 @@ import { init as settingsInit } from './src/endpoints/settings.js';
 import { redirectDeprecatedEndpoints, ServerStartup, setupPrivateEndpoints } from './src/server-startup.js';
 import { diskCache } from './src/endpoints/characters.js';
 
+// proxy
+import { bootstrap } from 'global-agent';
+
+
+if (process.env.GLOBAL_AGENT_HTTP_PROXY || process.env.GLOBAL_AGENT_HTTPS_PROXY) {
+    const proxy = process.env.GLOBAL_AGENT_HTTPS_PROXY || process.env.GLOBAL_AGENT_HTTP_PROXY;
+    console.info(`[global-agent] Proxy enabled: ${proxy}`);
+    bootstrap();
+} else {
+    console.info('[global-agent] No proxy configured, proceeding without proxy.');
+}
+
+
 // Unrestrict console logs display limit
 util.inspect.defaultOptions.maxArrayLength = null;
 util.inspect.defaultOptions.maxStringLength = null;
