2701f9c49bf62f30b856cc87b4de67d97e0d374d
ianchenx <nay99@foxmail.com>
2025-06-16 14:42:52 +0800

    groupIdApiAuthMiddleware support default leaprag config

--- src/middleware/groupIdAuth.js
+++ src/middleware/groupIdAuth.js
@@ -1,5 +1,8 @@
+import path from 'path';
 import { findUserInfoById } from '../db/user.js';
 import { getUserDirectories } from '../users.js';
+import { safeReadFileSync } from '../util.js';
+import { DEFAULT_USER, SETTINGS_FILE } from '../constants.js';
 
 export async function groupIdApiAuthMiddleware(request, response, next) {
     try {
@@ -25,6 +28,13 @@ export async function groupIdApiAuthMiddleware(request, response, next) {
         if (!user) {
             return response.status(401).json({ error: `Invalid group id ${groupId}` });
         }
+        const defaultDirectories = getUserDirectories(DEFAULT_USER.handle);
+        const pathToSettings = path.join(defaultDirectories.root, SETTINGS_FILE);
+        const fileContent = safeReadFileSync(pathToSettings, 'utf-8');
+        const settings = fileContent !== null ? JSON.parse(typeof fileContent === 'string' ? fileContent : fileContent.toString('utf-8')) : {};
+        const leaprag_api_url = settings?.power_user?.leaprag_api_url || '';
+        const leaprag_apikey = settings?.power_user?.leaprag_apikey || '';
+
 
         request.user = {
             profile: {
@@ -34,8 +44,8 @@ export async function groupIdApiAuthMiddleware(request, response, next) {
                 password: '',
                 salt: '',
                 admin: false,
-                leaprag_apikey: '',
-                leaprag_api_url: '',
+                leaprag_apikey,
+                leaprag_api_url,
             },
             directories: getUserDirectories(groupId),
         };
